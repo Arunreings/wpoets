7008d284e74904014546686bc7d3ad5ff7165c75
Arun Kumar <113786794+Arunreings@users.noreply.github.com>
2026-05-24 12:10:33 +0530

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,1 +1,10 @@
-
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "wpoet";
+$conn = mysqli_connect($host, $user, $password, $database);
+if (!$conn) {
+    die("Connection Failed : " . mysqli_connect_error());
+}
+?>
